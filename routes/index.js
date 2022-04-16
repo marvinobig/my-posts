d9e0e05c8af75404b9e8f9f4054bcd8eafe6c72a
Marvin obig <obig.marv@gmail.com>
2022-04-16 16:33:15 +0100

    add router post request & posts array

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,31 @@
 var express = require("express");
 var router = express.Router();
 
-const messages = [
+const posts = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date(),
+    added: new Date().toUTCString(),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date(),
+    added: new Date().toUTCString(),
   },
 ];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express", messages: messages });
+  res.render("index", { title: "My Posts", posts: posts });
+});
+
+router.post("/new", function (req, res, next) {
+  posts.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date().toUTCString(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
